b588192b67304059a1d7eb75fbc4b55804540955
rizkifadillah <rizkifadillah26.rf@gmail.com>
2025-01-28 13:57:29 +0700

    layout

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,42 @@
+<head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Dashboard - Analytics | Materio - Bootstrap Material Design Admin Template</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('materio-bootstrap-html-admin-template-free/assets/img/favicon/favicon.ico')}}" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap"
+      rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{ asset('materio-bootstrap-html-admin-template-free/assets/vendor/fonts/remixicon/remixicon.css')}}" />
+
+    <!-- Menu waves for no-customizer fix -->
+    <link rel="stylesheet" href="{{ asset('materio-bootstrap-html-admin-template-free/assets/vendor/libs/node-waves/node-waves.css')}}" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('materio-bootstrap-html-admin-template-free/assets/vendor/css/core.css')}}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('materio-bootstrap-html-admin-template-free/assets/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('materio-bootstrap-html-admin-template-free/assets/css/demo.css')}}" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('materio-bootstrap-html-admin-template-free/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+    <link rel="stylesheet" href="{{ asset('materio-bootstrap-html-admin-template-free/assets/vendor/libs/apex-charts/apex-charts.css')}}" />
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="{{ asset('materio-bootstrap-html-admin-template-free/assets/vendor/js/helpers.js')}}"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{ asset('materio-bootstrap-html-admin-template-free/assets/js/config.js')}}"></script>
+  </head>
